08361db98a5d25f3e34cc284462e4144e834766b
Munir <mu@nir.com>
2016-11-26 20:29:14 -0200

    Teste Comit

--- PowaNota/PowaNota/Util/Constantes.cs
+++ PowaNota/PowaNota/Util/Constantes.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 namespace Nota1.Util
 {
+    //constantes
    public  class Constantes
     {
         public static readonly String WS_HOST = "http://localhost:9999/PowaNota/WS";
