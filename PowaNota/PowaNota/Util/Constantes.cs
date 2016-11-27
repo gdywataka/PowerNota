cb53c48bbadc63b2c4698690a61ca20ff5330db3
Jose <jose@javaHibernateS2.com>
2016-11-27 20:06:35 -0200

    Add: constante para conexão alternativo WS

--- PowaNota/PowaNota/Util/Constantes.cs
+++ PowaNota/PowaNota/Util/Constantes.cs
@@ -10,5 +10,10 @@ namespace Nota1.Util
    public  class Constantes
     {
         public static readonly String WS_HOST = "http://localhost:9999/PowaNota/WS";
+
+        /// <summary>
+        /// Usada para ambientes alternativos
+        /// </summary>
+        public static readonly String WS_HOST_ALT = "http://127.0.0.1:8081/PowaNota/WS";
     }
 }
