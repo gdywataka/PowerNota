62b094a7b8eabf747414c6ab980af9210e46012c
Munir <mu@nir.com>
2016-11-28 17:27:07 -0200

    Arrumado campo de texto de Criar nota

--- PowaNota/PowaNota/Util/WebService.cs
+++ PowaNota/PowaNota/Util/WebService.cs
@@ -17,7 +17,7 @@ namespace Nota1.Util
             webCliente.Headers[HttpRequestHeader.ContentType] = "application/json";
             try
             {
-                webCliente.UploadString(C.WS_HOST_ALT + link, json);
+                webCliente.UploadString(C.WS_HOST + link, json);
             }
             catch (Exception)
             {
@@ -29,7 +29,7 @@ namespace Nota1.Util
         {
             try
             {
-               return webCliente.DownloadString(C.WS_HOST_ALT + link);
+               return webCliente.DownloadString(C.WS_HOST + link);
             }
             catch (Exception)
             {
