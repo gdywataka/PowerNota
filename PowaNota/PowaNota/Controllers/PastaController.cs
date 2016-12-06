b0f2bba19b660073723005ac9972c11d636b0334
Jose <jose@javaHibernateS2.com>
2016-12-06 01:39:15 -0200

    Mais estruturas da pasta

--- PowaNota/PowaNota/Controllers/PastaController.cs
+++ PowaNota/PowaNota/Controllers/PastaController.cs
@@ -10,23 +10,25 @@ using System.Web.Mvc;
 using Newtonsoft.Json.Linq;
 using sessao = Nota1.Sessao.Session;
 using Nota1.Models;
+using System.Text.RegularExpressions;
+using PowaNota.Util;
 
 namespace PowaNota.Controllers
 {
-    class PastaController : Controller
+    public class PastaController : Controller
     {
 
         public ActionResult List()
         {
             if (sessao.usuarioLogado != null)
             {
-                var jsonPastas = WebService.downloadJson("/Pasta/recuperar/recuperarPorIdUsuario/" + sessao.usuarioLogado.id.ToString());
+                var jsonPastas = WebService.downloadJson("/Pasta/recuperarPorIdUsuario/" + sessao.usuarioLogado.id.ToString());
                 GenericDTO dto = JsonConvert.DeserializeObject<GenericDTO>(jsonPastas);
                 dto.payload = ((JArray)dto.payload).ToObject<List<Pasta>>();
+                ViewBag.json_pastas = PastaJsonConverter.pastaConverter(((List<Pasta>)dto.payload));
             }
 
             ViewBag.tela = 0;
-            ViewBag.json_pastas = "";
             return View();
         }
     }
---
+++ PowaNota/PowaNota/Util/PastaJsonConverter.cs
@@ -0,0 +1,36 @@
+﻿using Nota1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PowaNota.Util
+{
+    class PastaJsonConverter
+    {
+        public PastaJsonConverter()
+        {
+
+        }
+
+        public static String pastaConverter(List<Pasta> pastas)
+        {
+            string json = null;
+
+            if(pastas == null)
+            {
+                return null;
+            }
+            foreach(Pasta p in pastas)
+            {
+                json = String.Concat
+                    ("{ 'id': ",p.id.ToString(),
+                    ",'parent':",p.pastaPai == null? "'#'" : p.pastaPai.ToString(),
+                    ",'text':'",p.nome,"',",
+                    "'icon':'glyphicon glyphicon-folder-open'},");
+            }
+            return json;
+        }
+    }
+}
